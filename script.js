d03d6b34113488a10e11285fd926c30e8a753f63
Vaishali7599 <140495282+Vaishali7599@users.noreply.github.com>
2024-09-16 14:03:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let change_color = document.getElementById("change_button")
+let reset = document.getElementById("reset_button");
+
+change_color.addEventListener("click", (e) => {
+    e.preventDefault()
+    var blockId = document.getElementById("block_id").value;
+    console.log(blockId);
+    var color = document.getElementById("colour_id").value;
+    console.log(color);
+
+    var cell = document.getElementById(blockId);
+
+    cell.style.backgroundColor = color;
+})
+
+reset.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    var gridClas = document.querySelectorAll(".grid-item");
+
+    gridClas.forEach(t => {
+        t.style.backgroundColor = 'transparent'
+    })
+})
+
+
